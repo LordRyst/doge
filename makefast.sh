58ca5c5c8208224e47a3f9900b6e98b30e207875
Richard Stockham <rickstockham@gmail.com>
2016-06-12 19:06:17 -0600

    added makefast.sh and updated messages

---
+++ makefast.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ccache g++ -Ofast -march=native -Wall -Werror -o server.out messages.cpp mongoose.c websocket_chat.c players.cpp amap.cpp game.cpp
+
+if [ $? = 0 ]; then
+    cppcheck .
+    if [ $? = 0 ]; then
+	valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./server.out
+	if [ $? = 0 ]; then
+	    git status
+	fi
+    fi
+fi
