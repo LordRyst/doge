7c8344ecb862da0259b56bba3f22f85a140077c9
Richard Stockham <rickstockham@gmail.com>
2016-06-27 18:41:06 -0600

    added forward message

--- messages.h
+++ messages.h
@@ -1,9 +1,15 @@
 #ifndef _MESSAGES_H_
 #define _MESSAGES_H_
 
+#include <vector>
+
 using namespace std;
 
+struct message {
+  void* mg_conn;
+  char* data;
+};
+
 void MessagesAdd(void* mg_conn, char* data, int size);
-void messages1Byte(void* mg_conn, char* data, int size);
 
 #endif
